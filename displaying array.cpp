d4cffa750e42783de58dfe533581249dafa97a60
05him <72925668+05him@users.noreply.github.com>
2020-10-21 19:45:35 +0530

    Create displaying array.cpp

---
+++ displaying array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numbers[5] = {7, 5, 6, 12, 35};
+
+    cout << "The numbers are: ";
+
+    //  Printing array elements
+    // using range based for loop
+    for (const int &n : numbers) {
+        cout << n << "  ";
+    }
+
+
+    cout << "\nThe numbers are: ";
+
+    //  Printing array elements
+    // using traditional for loop
+    for (int i = 0; i < 5; ++i) {
+        cout << numbers[i] << "  ";
+    }
+
+    return 0;
+}
